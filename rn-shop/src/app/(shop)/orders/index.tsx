facac95ac3a8d5cd379de5bb4bada442c6227c24
Gishgiish <gishnora@gmail.com>
2024-10-30 16:13:24 +0300

    commit

---
+++ rn-shop/src/app/(shop)/orders/index.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Orders = () => {
+  return (
+    <View >
+      <View>
+        <Text>Orders </Text>
+       
+      </View>
+    </View>
+  );
+};
+
+export default Orders;
+
+const styles = StyleSheet.create({});
